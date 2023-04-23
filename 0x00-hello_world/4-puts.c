81df278ce2c3cadf179b75ce613574874a3da971
KenyanGem <mwendechristine84@gmail.com>
2023-04-23 10:27:59 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#Include <stdio.h
+/**
+ * main A great program has two lines
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
